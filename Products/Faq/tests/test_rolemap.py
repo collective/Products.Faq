f978e975bbb46675ab768a804f5da39166587566
hvelarde <hector.velarde@gmail.com>
2013-10-17 09:31:01 -0300

    Fix test for Plone 4.0

--- Products/Faq/tests/test_rolemap.py
+++ Products/Faq/tests/test_rolemap.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
-import unittest2 as unittest
-from plone.app.testing import setRoles, TEST_USER_ID
+
 from Products.Faq.testing import PLONE_FAQ_INTEGRATION_TESTING
+
+import pkg_resources
+import unittest2 as unittest
+
+PLONE_VERSION = pkg_resources.require("Plone")[0].version
+FOLDER_ROLES = ['Contributor', 'Manager', 'Site Administrator']
+FAQ_ROLES = ['Contributor', 'Manager', 'Site Administrator']
+
+if PLONE_VERSION < '4.1':  # no 'Site Administrator' role
+    FOLDER_ROLES.remove('Site Administrator')
+    FAQ_ROLES.remove('Site Administrator')
 
 
 class RolemapTestCase(unittest.TestCase):
@@ -13,6 +23,6 @@ class RolemapTestCase(unittest.TestCase):
 
     def test_faq_roles(self):
         folder_roles = [r['name'] for r in self.portal.rolesOfPermission('Products.Faq: Add FaqFolder') if r['selected']]
-        self.assertEqual(folder_roles, ['Contributor', 'Manager', 'Site Administrator'])
+        self.assertEqual(folder_roles, FOLDER_ROLES)
         faq_roles = [r['name'] for r in self.portal.rolesOfPermission('Products.Faq: Add FaqEntry') if r['selected']]
-        self.assertEqual(folder_roles, ['Contributor', 'Manager', 'Site Administrator'])
+        self.assertEqual(faq_roles, FAQ_ROLES)
